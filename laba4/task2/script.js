53acf6dcbdd3b10dc832b5c034b683ffaaabd36f
AlphaFloppa <cyril.bakhtin@yandex.ru>
2025-03-13 21:44:34 +0300

    a

--- laba4/task2/script.js
+++ laba4/task2/script.js
@@ -1,26 +1,29 @@
-let description = document.querySelector(".post-user-description");
-let show_more_button = document.querySelector(".reveal-description-button");
-let short_description = document.querySelector(".post-short-user-description");
-show_more_button.addEventListener("click", function(){
-    show_more_button.remove();
-    short_description.remove();
-    description.style.position = "static";
-    description.style.visibility = "visible";    
-})
-let likes_button = document.querySelector(".likes-button");
-let like_symbol = document.querySelector(".like-symbol");
-let likes_count = document.querySelector(".likes-count");
-likes_button.IsActive = false;
-likes_button.addEventListener("click", function(){
-    if(!likes_button.IsActive){
-        like_symbol.style.color = "white";
-        likes_count.style.color = "white";
-        likes_button.style.backgroundColor = "red";
-    }
-    else{
-        like_symbol.style.color = "red";
-        likes_count.style.color = "black";
-        likes_button.style.backgroundColor = "rgba(34, 34, 34, 0.05)";
-    }
-    likes_button.IsActive = !likes_button.IsActive;
-})
+let descriptions = document.querySelectorAll(".post-user-description");
+let show_more_buttons = document.querySelectorAll(".reveal-description-button");
+let short_descriptions = document.querySelectorAll(".post-short-user-description");
+for(let i=0; i<show_more_buttons.length; ++i)
+    show_more_buttons[i].addEventListener("click", function(){
+        show_more_buttons[i].remove();
+        short_descriptions[i].remove();
+        descriptions[i].style.position = "static";
+        descriptions[i].style.visibility = "visible";    
+    })
+let likes_buttons = document.querySelectorAll(".likes-button");
+let like_symbols = document.querySelectorAll(".like-symbol");
+let likes_counts = document.querySelectorAll(".likes-count");
+likes_buttons.forEach(
+    (item) => {item.IsActive = false;})
+for(let i=0; i<likes_buttons.length; ++i)
+    likes_buttons[i].addEventListener("click", function(){
+        if(!likes_buttons[i].IsActive){
+            like_symbols[i].style.color = "white";
+            likes_counts[i].style.color = "white";
+            likes_buttons[i].style.backgroundColor = "red";
+        }
+        else{
+            like_symbols[i].style.color = "red";
+            likes_counts[i].style.color = "black";
+            likes_buttons[i].style.backgroundColor = "rgba(34, 34, 34, 0.05)";
+        }
+        likes_buttons[i].IsActive = !likes_buttons[i].IsActive;
+    })
