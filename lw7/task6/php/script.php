26ad24dc8a2c1f097a20ada09e8655abf100c586
AlphaFloppa <cyril.bakhtin@yandex.ru>
2025-04-19 00:55:34 +0300

    a

--- lw7/task6/php/script.php
+++ lw7/task6/php/script.php
@@ -1,18 +1,27 @@
 <?php
-function FindUserById(): bool{                      
-   if(!isset($_GET["id"])) return false;
-    $id = intval(trim($_GET["id"]));
-    if(preg_match("[.*\D+.*]", trim($_GET["id"])) == 1 || $id == 0) return false;         //если найден нецифровой символ        
-    $users_data = json_decode(file_get_contents("../data/users-data.json"), true);
-    foreach($users_data as $i)
-    { 
-        if($i["id"] == $id)
+function DefineEnding($count): string
+{
+    if(($count%100 <= 15 && $count%100 >=11)) return "постов";
+    switch($count%10)
         {
-            echo file_get_contents($users_data[$id-1]["profile-page-path"]);
-            return true;   
+            case 1: return"пост";
+            case 2:
+            case 3:
+            case 4: return "поста";
+            default: return"постов";
         }
-    }
-    return false;
 }
 
-FindUserById();
+if(isset($_GET["id"])) 
+    $id = $_GET["id"];
+$data = json_decode(file_get_contents("../data/users-data.json"), true);
+$user_avatar = $data[$id-1]["user-avatar-path"];
+$user_name = $data[$id-1]["user-name"];
+$user_status = $data[$id-1]["user-status"];
+$user_posts_preview = $data[$id-1]["posts-preview-imgs-path"];
+$posts_count = count($user_posts_preview);
+$posts_count_describe = DefineEnding($posts_count);
+$posts_preview_links_list = "";
+foreach($user_posts_preview as $i)
+    $posts_preview_links_list .= "<img src = \"" . $i . "\" alt = \"Try Ctrl-R\" class = \"post-img\">";
+include "../user-profile-tpl/index.phtml";
