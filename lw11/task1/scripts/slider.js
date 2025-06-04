4e6cb366a2d7a1bfd50ea7629ab76b553f533cec
daizaloginitsya <kibersup227@gmail.com>
2025-06-04 11:36:26 +0300

    a

--- lw11/task1/scripts/slider.js
+++ lw11/task1/scripts/slider.js
@@ -6,14 +6,18 @@ window.addEventListener("load", function(){
         let indexer = slider_item.querySelector(".post-number");
         let switch_forward_btn = slider_item.querySelector(".switch-buttons > .next-photo-button");
         let switch_back_btn = slider_item.querySelector(".switch-buttons > .previous-photo-button");
-        gallery.firstElementChild.classList.add("active");
-        indexer.textContent = "1/" + gallery.childElementCount;
+        if(slider_item.querySelector(".post-photo.active") == null){
+            gallery.firstElementChild.classList.add("active");
+            indexer.textContent = "1/" + gallery.childElementCount;
+        }
+        gallery.querySelector(".active").addEventListener("click", A);
         switch_forward_btn.addEventListener("click", function(){
             Switch(gallery.firstElementChild, "next")}
         );
         switch_back_btn.addEventListener("click", function(){
             Switch(gallery.firstElementChild, "previous")}
         );
+    }
     /**
  * @description организует логику перехода на другое фото и возможности открытия в модальном окне 
  * @param {photo_html_code} node - разметка фото
@@ -26,23 +30,23 @@ window.addEventListener("load", function(){
             return null;
         }
         node.classList.remove("active");
-        let indexer = node.parentNode.nextElementSibling;
-        node.removeEventListener("click", function(event){ViewInModal(event.target.parentNode.parentNode)});
+        node.removeEventListener("click", A);
+        let indexer = node.parentNode.parentNode.querySelector(".post-number");
         if(direction == "next")
         {
             if(node.nextElementSibling != null && node.nextElementSibling != undefined){
                 node.nextElementSibling.classList.add("active");
                 indexer.textContent = (parseInt(
                     indexer.textContent.split("/")[0]) + 1) + "/" + node.parentNode.childElementCount;
-                    if(!node.parentNode.parentNode.classList.contains("modal"))
-                        node.nextElementSibling.addEventListener("click", function(event){ViewInModal(event.target.parentNode.parentNode)});
+                if(document.querySelector(".modal-window") == null)
+                    node.nextElementSibling.addEventListener("click", A);
                     //    если оно уже не открыто как модальное
             }
             else{
                 node.parentNode.firstElementChild.classList.add("active");
                 node.parentNode.nextElementSibling.textContent = 1 + "/" + node.parentNode.childElementCount;
-                if(!node.parentNode.parentNode.classList.contains("modal"))
-                    node.parentNode.firstElementChild.addEventListener("click", function(event){ViewInModal(event.target.parentNode.parentNode)});
+                if(document.querySelector(".modal-window") == null)
+                    node.parentNode.firstElementChild.addEventListener("click", A);
                 //передаем в функцию именно слайдер
             }
 
@@ -53,29 +57,57 @@ window.addEventListener("load", function(){
                 node.previousElementSibling.classList.add("active");
                 indexer.textContent = (parseInt(
                     indexer.textContent.split("/")[0]) - 1) + "/" + node.parentNode.childElementCount;
-                if(!node.parentNode.parentNode.classList.contains("modal"))
-                    node.previousElementSibling.addEventListener("click", function(event){ViewInModal(event.target.parentNode.parentNode)});
+                if(document.querySelector(".modal-window") == null)
+                    node.previousElementSibling.addEventListener("click", A);
             }
             else{
                 node.parentNode.lastElementChild.classList.add("active");
                 indexer.textContent = node.parentNode.childElementCount + "/" + node.parentNode.childElementCount;
-                if(!node.parentNode.parentNode.classList.contains("modal"))
-                    node.parentNode.lastElementChild.addEventListener("click", function(event){ViewInModal(event.target.parentNode.parentNode)});
+                if(document.querySelector(".modal-window") == null)
+                    node.parentNode.lastElementChild.addEventListener("click", A);
             }
         }      
-        /**
-         * @description навешивает на копию слайдера класс modal и пихает ее в body чтобы стилизовать в style.css в модальном окне, также добавляет доп интерфейс присущий модальному окну
-         * @description т.к это копия то для нее остаются обработчики событий для кнопок и прогресс просмотра при выходе из модального окна 
-         * @param {slider_html_code} slider - объект с разметкой слайдера 
-         */
-        function ViewInModal(slider){
-            let modal_window = slider;
-            let exit_btn = document.createElement("button");
-            exit_btn.classList.add("modal-window-exit-btn");
-            exit_btn.innerHTML = "<img src = \"Plus 24.png\"/>"
-            modal_window.classList.add("modal");
-            document.querySelector("body").appendChild(modal_window);
+    }
+    /**
+    * @description навешивает на копию слайдера класс modal, заново навешивает поведение слайдера и пихает ее в body чтобы стилизовать в style.css в модальном окне, также добавляет возможность выхода
+    * @description прогресс просмотра при выходе из модального окна остается
+    * @param {slider_html_code} slider - объект с разметкой слайдера 
+    */
+    function ViewInModal(slider){
+        if(document.querySelector(".modal-window") != null) return null;
+        let modal_slider = slider.cloneNode(true);
+        Slider(modal_slider);
+        let exit_btn = document.createElement("button");
+        let modal_window = document.createElement("div");
+        let modal_layer = document.createElement("div");
+        modal_layer.classList.add("modal-layer");
+        exit_btn.classList.add("modal-window-exit-btn");
+        exit_btn.addEventListener("click", function(){RemoveModal(slider)});
+        document.addEventListener("keydown", function(event){
+            if(event.code == "Escape"){
+                RemoveModal(slider);
+            }
+        });
+        modal_slider.prepend(exit_btn);
+        modal_slider.classList.add("modal");
+        modal_window.classList.add("modal-window");
+        modal_window.prepend(modal_layer);
+        modal_window.append(modal_slider);
+        modal_window.style.setProperty("transform", "translateY(" + window.pageYOffset + "px)");
+        document.querySelector("body").prepend(modal_window);
+        document.querySelector("body").style.overflow = "hidden";
+        document.querySelector(".modal-layer").addEventListener("click", function(){RemoveModal(slider)});
+    }
+
+    function A(event){ViewInModal(event.target.parentNode.parentNode);}
+    function RemoveModal(slider){
+        if(document.querySelector(".modal-window") != null){
+            slider.querySelector(".post-number").textContent = document.querySelector(".modal-window .post-number").textContent;
+            slider.querySelector(".post-photos-gallery").innerHTML = document.querySelector(".modal-window .post-photos-gallery").innerHTML;
+            Slider(slider);             
+            //выгрузка прогресса в основной слайдер путем копирования разметки галереи с active классами и индексатора
+            document.querySelector("body").removeChild(document.querySelector(".modal-window"));
+            document.querySelector("body").style.overflow = "visible";    
         }
     }
-}
 });
